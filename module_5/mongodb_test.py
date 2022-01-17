ea05385f5aff4faad678722cb88f781bf853f63f
cerahbes <93489662+cerahbes@users.noreply.github.com>
2022-01-17 01:22:06 -0500

    Update mongodb_test.py

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,2 +1,7 @@
-print " -- Pytech COllection List -- "
-print "['students']"
+import pymongo
+from pymongo import MongoClient
+url = "mongodb+srv://admin:admin@cluster0.nie00.mongodb.net/pytech?retryWrites=true&w=majority";
+client = MongoClient(url)
+db = client.students
+print(" -- Pytech COllection List -- ")
+print(db.list_collection_names)
