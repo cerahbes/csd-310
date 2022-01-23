d03e2b49521b40a73cfb8c2207f07596bab38ac8
cerahbes <93489662+cerahbes@users.noreply.github.com>
2022-01-23 18:17:32 -0500

    Delete mongodb_test.py

--- module_5/mongodb_test.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-from pymongo import MongoClient
-url = "mongodb+srv://admin:admin@cluster0.nie00.mongodb.net/pytech?retryWrites=true&w=majority";
-client = MongoClient(url)
-db = client.students
-print(" -- Pytech COllection List -- ")
-print(db.list_collection_names)
